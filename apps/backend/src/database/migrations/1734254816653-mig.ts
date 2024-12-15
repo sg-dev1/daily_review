dc65e24b92ae007a42e70830aa8795af3ef0606c
Stefan G <sg-dev1@users.noreply.github.com>
2024-12-15 10:30:29 +0100

    Fixed migration command, typeorm db scripts are now working

--- apps/backend/src/database/migrations/1734254816653-mig.ts
+++ apps/backend/src/database/migrations/1734254816653-mig.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Mig1732528002752 implements MigrationInterface {
-    name = 'Mig1732528002752'
+export class Mig1734254816653 implements MigrationInterface {
+    name = 'Mig1734254816653'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "user_entity" ("id" SERIAL NOT NULL, "email" character varying NOT NULL, "username" character varying NOT NULL, "password" character varying NOT NULL, CONSTRAINT "UQ_415c35b9b3b6fe45a3b065030f5" UNIQUE ("email"), CONSTRAINT "PK_b54f8ea623b17094db7667d8206" PRIMARY KEY ("id"))`);
